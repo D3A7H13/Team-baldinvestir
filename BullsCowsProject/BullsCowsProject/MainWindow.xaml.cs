93f8ba8d6833f6e1881d76d13dcaaf852e46d2b8
7x3 D3A7H <s_sheitanov@abv.bg>
2015-10-01 14:39:56 +0300

    Added more animations.
    Only the bulls and the header left

--- BullsCowsProject/BullsCowsProject/MainWindow.xaml.cs
+++ BullsCowsProject/BullsCowsProject/MainWindow.xaml.cs
@@ -163,20 +163,9 @@ namespace BullsCowsProject
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            
-            /*DoubleAnimation ne = new DoubleAnimation();
-            ne.From = 0;
-            ne.To = 40;
-            ne.Duration = new Duration(TimeSpan.FromSeconds(0.500));
-            ne.RepeatBehavior = new RepeatBehavior(1);
-            ne.AutoReverse = true;
-            TranslateTransform tt = new TranslateTransform();
-            checkButton.RenderTransform = tt;
-            tt.BeginAnimation(TranslateTransform.YProperty, ne);
-            */
             DispatcherTimer dispatcherTimer = new DispatcherTimer();
             dispatcherTimer.Tick += dispatcherTimer_Tick;
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
             
             dispatcherTimer.Start();
 
@@ -185,33 +174,289 @@ namespace BullsCowsProject
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
             (sender as DispatcherTimer).Stop();
+            AnimateHistoryTab();
+            AnimateHitoryLabel();
+            AnimateCheckButton();
+            AnimateRandomNumber();
+            AnimateInputBox();
+            AnimateFirstCow();
+            AnimateSecondCow();
+            AnimateThirdCow();
+            AnimateForthCow();
+
+        }
+
+        private void secondAnimationForthCowStart(object sender, EventArgs e)
+        {
+            (sender as DispatcherTimer).Stop();
+
+            double oldLeft = cow4.Margin.Left;
+            double oldRight = cow4.Margin.Right;
+            double oldTop = cow4.Margin.Top;
+            double oldBottom = cow4.Margin.Bottom;
+
+            cow4.Margin = new Thickness(oldLeft, oldTop + 45, oldRight, oldBottom);
+
+            DoubleAnimation rotate = new DoubleAnimation();
+            rotate.From = 0;
+            rotate.To = 45;
+            rotate.AutoReverse = false;
+            rotate.Duration = new Duration(TimeSpan.FromSeconds(0.500));
+
+            RotateTransform rt = new RotateTransform();
 
             DoubleAnimation da = new DoubleAnimation();
             da.From = 0;
-            da.To = 360;
+            da.To = -2;
             da.AutoReverse = false;
-            da.Duration = new Duration(TimeSpan.FromSeconds(5.000));
-            da.RepeatBehavior = RepeatBehavior.Forever;
-            DoubleAnimation ne = new DoubleAnimation();
-            ne.From = 0;
-            ne.To = 100;
-            ne.Duration = new Duration(TimeSpan.FromSeconds(0.500));
-            ne.RepeatBehavior = RepeatBehavior.Forever;
-            ne.AutoReverse = true;
+            da.Duration = new Duration(TimeSpan.FromSeconds(1.000));
+
             TranslateTransform tt = new TranslateTransform();
-            
-            
-            RotateTransform rt = new RotateTransform();
+
             TransformGroup tg = new TransformGroup();
             tg.Children.Add(rt);
             tg.Children.Add(tt);
-            
-            lblHello.RenderTransform = tg;
 
-            rt.BeginAnimation(RotateTransform.AngleProperty, da);
-            tt.BeginAnimation(TranslateTransform.YProperty, ne);
-            //tg.BeginAnimation(RotateTransform.AngleProperty, da);
-            //tg.BeginAnimation(TranslateTransform.YProperty, ne);
+            cow4.RenderTransform = tg;
+            rt.BeginAnimation(RotateTransform.AngleProperty, rotate);
+            tt.BeginAnimation(TranslateTransform.YProperty, da);
+           
+        }
+
+        private void AnimateForthCow()
+        {
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = 0;
+            da.To = 50;
+            da.AutoReverse = false;
+            da.Duration = new Duration(TimeSpan.FromSeconds(1.000));
+
+            TranslateTransform tt = new TranslateTransform();
+            cow4.RenderTransform = tt;
+            tt.BeginAnimation(TranslateTransform.YProperty, da);
+
+            DispatcherTimer secondAnimationForthCow = new DispatcherTimer();
+            secondAnimationForthCow.Tick += secondAnimationForthCowStart;
+            secondAnimationForthCow.Interval = new TimeSpan(0, 0, 0, 1, 0);
+
+            secondAnimationForthCow.Start();
+        }
+
+        private void secondAnimationThirdCowStart(object sender, EventArgs e)
+        {
+            (sender as DispatcherTimer).Stop();
+
+            double oldLeft = cow3.Margin.Left;
+            double oldRight = cow3.Margin.Right;
+            double oldTop = cow3.Margin.Top;
+            double oldBottom = cow3.Margin.Bottom;
+
+            cow3.Margin = new Thickness(oldLeft, oldTop + 50, oldRight, oldBottom);
+
+            DoubleAnimation rotate = new DoubleAnimation();
+            rotate.From = 0;
+            rotate.To = -36;
+            rotate.AutoReverse = false;
+            rotate.Duration = new Duration(TimeSpan.FromSeconds(0.500));
+
+            RotateTransform rt = new RotateTransform();
+            cow3.RenderTransform = rt;
+            rt.BeginAnimation(RotateTransform.AngleProperty, rotate);
+        }
+
+        private void AnimateThirdCow()
+        {
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = 0;
+            da.To = 50;
+            da.AutoReverse = false;
+            da.Duration = new Duration(TimeSpan.FromSeconds(1.000));
+
+            TranslateTransform tt = new TranslateTransform();
+            cow3.RenderTransform = tt;
+            tt.BeginAnimation(TranslateTransform.YProperty, da);
+
+            DispatcherTimer secondAnimationThirdCow = new DispatcherTimer();
+            secondAnimationThirdCow.Tick += secondAnimationThirdCowStart;
+            secondAnimationThirdCow.Interval = new TimeSpan(0, 0, 0, 1, 0);
+
+            secondAnimationThirdCow.Start();
+        }
+
+        private void secondAnimationSecondCowStart(object sender, EventArgs e)
+        {
+            (sender as DispatcherTimer).Stop();
+
+            double oldLeft = cow2.Margin.Left;
+            double oldRight = cow2.Margin.Right;
+            double oldTop = cow2.Margin.Top;
+            double oldBottom = cow2.Margin.Bottom;
+
+            cow2.Margin = new Thickness(oldLeft, oldTop + 37, oldRight, oldBottom);
+
+            DoubleAnimation rotate = new DoubleAnimation();
+            rotate.From = 0;
+            rotate.To = 24;
+            rotate.AutoReverse = false;
+            rotate.Duration = new Duration(TimeSpan.FromSeconds(0.500));
+
+            RotateTransform rt = new RotateTransform();
+            cow2.RenderTransform = rt;
+            rt.BeginAnimation(RotateTransform.AngleProperty, rotate);
+        }
+
+        private void AnimateSecondCow()
+        {
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = 0;
+            da.To = 37;
+            da.AutoReverse = false;
+            da.Duration = new Duration(TimeSpan.FromSeconds(1.000));
+
+            TranslateTransform tt = new TranslateTransform();
+            cow2.RenderTransform = tt;
+            tt.BeginAnimation(TranslateTransform.YProperty, da);
+
+            DispatcherTimer secondAnimationSecondCow = new DispatcherTimer();
+            secondAnimationSecondCow.Tick += secondAnimationSecondCowStart;
+            secondAnimationSecondCow.Interval = new TimeSpan(0, 0, 0, 1, 0);
+
+            secondAnimationSecondCow.Start();
+        }
+
+        private void secondAnimationFirstCowStart(object sender, EventArgs e)
+        {
+            (sender as DispatcherTimer).Stop();
+
+            double oldLeft = cow.Margin.Left;
+            double oldRight = cow.Margin.Right;
+            double oldTop = cow.Margin.Top;
+            double oldBottom = cow.Margin.Bottom;
+
+            cow.Margin = new Thickness(oldLeft, oldTop + 35, oldRight, oldBottom);
+
+            DoubleAnimation rotate = new DoubleAnimation();
+            rotate.From = 0;
+            rotate.To = -80;
+            rotate.AutoReverse = false;
+            rotate.Duration = new Duration(TimeSpan.FromSeconds(0.500));
+
+            RotateTransform rt = new RotateTransform();
+            cow.RenderTransform = rt;
+            rt.BeginAnimation(RotateTransform.AngleProperty, rotate);
+        }
+
+        private void AnimateFirstCow()
+        {
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = 0;
+            da.To = 35;
+            da.AutoReverse = false;
+            da.Duration = new Duration(TimeSpan.FromSeconds(1.000));
+
+            TranslateTransform tt = new TranslateTransform();
+            cow.RenderTransform = tt;
+            tt.BeginAnimation(TranslateTransform.YProperty, da);
+
+            DispatcherTimer secondAnimationFirstCow = new DispatcherTimer();
+            secondAnimationFirstCow.Tick += secondAnimationFirstCowStart;
+            secondAnimationFirstCow.Interval = new TimeSpan(0, 0, 0, 1, 0);
+
+            secondAnimationFirstCow.Start();
+        }
+
+        private void AnimateInputBox()
+        {
+            DoubleAnimation drop = new DoubleAnimation();
+            drop.From = 0;
+            drop.To = 76;
+            drop.AutoReverse = false;
+            drop.Duration = new Duration(TimeSpan.FromSeconds(1.000));
+            drop.FillBehavior = FillBehavior.HoldEnd;
+            TranslateTransform tt = new TranslateTransform();
+            inputTextBox.RenderTransform = tt;
+            tt.BeginAnimation(TranslateTransform.YProperty, drop);
+            
+
+            DispatcherTimer secondAnimationInputBox = new DispatcherTimer();
+            secondAnimationInputBox.Tick += secondAnimationInputBoxStart;
+            secondAnimationInputBox.Interval = new TimeSpan(0, 0, 0, 1, 0);
+
+            secondAnimationInputBox.Start();
+        }
+
+        private void secondAnimationInputBoxStart(object sender, EventArgs e)
+        {
+            (sender as DispatcherTimer).Stop();
+
+            double oldLeft = inputTextBox.Margin.Left;
+            double oldRight = inputTextBox.Margin.Right;
+            double oldTop = inputTextBox.Margin.Top;
+            double oldBottom = inputTextBox.Margin.Bottom;
+
+            inputTextBox.Margin = new Thickness(oldLeft, oldTop + 76, oldRight, oldBottom);
+
+            DoubleAnimation rotate = new DoubleAnimation();
+            rotate.From = 0;
+            rotate.To = -5;
+            rotate.AutoReverse = false;
+            rotate.Duration = new Duration(TimeSpan.FromSeconds(0.500));
+
+            RotateTransform rt = new RotateTransform();
+            inputTextBox.RenderTransform = rt;
+            rt.BeginAnimation(RotateTransform.AngleProperty, rotate);
+        }
+
+        private void AnimateRandomNumber()
+        {
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = 0;
+            da.To = 46;
+            da.AutoReverse = false;
+            da.Duration = new Duration(TimeSpan.FromSeconds(1.000));
+
+            TranslateTransform tt = new TranslateTransform();
+            randomNumLabel.RenderTransform = tt;
+            tt.BeginAnimation(TranslateTransform.YProperty, da);
+        }
+
+        private void AnimateCheckButton()
+        {
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = 0;
+            da.To = 68;
+            da.AutoReverse = false;
+            da.Duration = new Duration(TimeSpan.FromSeconds(1.000));
+
+            TranslateTransform tt = new TranslateTransform();
+            checkButton.RenderTransform = tt;
+            tt.BeginAnimation(TranslateTransform.YProperty, da);
+        }
+
+        private void AnimateHitoryLabel()
+        {
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = 0;
+            da.To = 20;
+            da.AutoReverse = false;
+            da.Duration = new Duration(TimeSpan.FromSeconds(1.000));
+
+            TranslateTransform tt = new TranslateTransform();
+            HistoryLabel.RenderTransform = tt;
+            tt.BeginAnimation(TranslateTransform.YProperty, da);
+        }
+
+        private void AnimateHistoryTab()
+        {
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = 0;
+            da.To = 10;
+            da.AutoReverse = false;
+            da.Duration = new Duration(TimeSpan.FromSeconds(1.000));
+
+            TranslateTransform tt = new TranslateTransform();
+            historyListBox.RenderTransform = tt;
+            tt.BeginAnimation(TranslateTransform.YProperty, da);
         }
     }
 }
